b551eb945cd96b466ab949fc0736c40fb8109029
salamandaaa <hht2010ily@hotmail.com>
2022-03-28 10:36:36 +0000

    near to Avalanche

---
+++ avalancheToNear.js
@@ -0,0 +1,55 @@
+const blockchain = require('./blockchain.js');
+const { program } = require('commander');
+
+async function sendGreeting() {
+  ///////////////////////////////////////////////
+  ///////    Avalanche To PlatON     ////////////
+  ///////////////////////////////////////////////
+
+  // send greeting to smart contract on PlatON
+  await blockchain.sendMessageFromAvalancheToNear();
+
+  // query greeting from smart contract on PlatON
+  console.log('Wait for the message to be synchronized.');
+
+  setTimeout(async () => {
+    const message = await blockchain.queryMessageFromPlatON();
+    console.log(message);
+  }, 30 * 1000);
+}
+
+async function sendOCTask(nums) {
+  ///////////////////////////////////////////////
+  ///////    Avalanche To PlatON     ////////////
+  ///////////////////////////////////////////////
+
+  // send outsourcing computing task to smart contract from Avalanche to PlatON
+  await blockchain.sendOCTaskToPlatON(nums);
+
+  // query greeting from smart contract on Avalanche
+  console.log('Wait for the message to be synchronized.');
+
+  setTimeout(async () => {
+    const message = await blockchain.queryOCResultFromAvalanche();
+    console.log(message);
+  }, 60 * 1000);
+}
+
+(async function() {
+	function list(val) {
+		return val.split(',')
+	}
+
+  program
+	  .version('0.1.0')
+	  .option('-g, --greet', 'send greeting to PlatON')
+	  .option('-c, --compute <num1, ..., numn>', 'send outsourcing computing task to PlatON', list)
+	  .parse(process.argv);
+
+  if (program.opts().greet) {
+    await sendGreeting();
+  }
+  else if (program.opts().compute) {
+    await sendOCTask(program.opts().compute);
+  }
+})();
--- blockchain.js
+++ blockchain.js
@@ -1,7 +1,10 @@
 const Web3 = require('web3');
 const fs = require('fs');
+const path = require('path');
+const homedir = require("os").homedir();
 const avalanche = require('./deploy/avalanche');
 const platon = require('./deploy/platon');
+const near = require('./deploy/near')
 
 const avalancheWeb3 = new Web3('https://api.avax-test.network/ext/bc/C/rpc');
 const platONWeb3 = new Web3('http://35.247.155.162:6789');
@@ -24,6 +27,18 @@ let avalancheContract = new avalancheWeb3.eth.Contract(greetingAbi, avalancheCon
 let platonContractAddress = '0xF31562eF36Ffa449CEbdD1eC97c94aFa9D2C6862';
 let platonContract = new platONWeb3.eth.Contract(greetingAbi, platonContractAddress);
 
+// NEAR contract
+let nearContractId = 'greeting.datlocker.testnet';
+let nearSumContractId = "sum.datlocker.testnet";
+let nearSender = "shanks.testnet";
+let nearNetworkId = "testnet";
+// const keyFilePath = path.resolve(
+//   homedir,
+//   `./.near-credentials/${nearNetworkId}/${nearSender}.json`
+// );
+// const keyFile = require(keyFilePath);
+const SenderPrivateKey = "ed25519:2ujXoT1SktY2tspiAMimY5ZEji1MNdP1fRCUPqzrpdzkkQ7JkPVDg9nS5BV5Yefb2GgHqaE8anC1KfhGLJmmU3Af";
+
 // Get current date
 function getCurrentDate() {
   var today = new Date();
@@ -38,14 +53,31 @@ module.exports = {
     // Cross-chain message delivering from `PlatON` to `Avalanche`. Send greeting to smart contract of `Avalanche`. 
     await platon.sendTransaction(platonContract, 'sendGreeting', testAccountPrivateKey, ['AVALANCHE', ['PlatONEVMDEV', 'Greetings', 'Greeting from PlatON', getCurrentDate()]]);
   },
+  
+  async sendMessageFromNearToAvalance() {
+    await near.sendTransaction(nearContractId, nearSender, SenderPrivateKey, "send_greeting", {to_chain: 'AVALANCHE', title: 'Greetings', content: 'Greeting from NEAR', date: getCurrentDate()})
+  },
+
+  async sendOCTaskFromNearToAvalanche(nums) {
+    // Cross-chain call delivering from `PlatON` to `Avalanche`.
+    await near.sendTransaction(nearSumContractId, nearSender, SenderPrivateKey, 'sum', {to_chain: 'AVALANCHE', params_vector: nums});
+  },
+
   async sendMessageToPlatON() {
     // Cross-chain message delivering from `Avalanche` to `PlatON`. Send greeting to smart contract of `PlatON`
     await avalanche.sendTransaction(avalancheContract, 'sendGreeting', testAccountPrivateKey, ['PlatONEVMDEV', ['AVALANCHE', 'Greetings', 'Greeting from Avalanche', getCurrentDate()]]);
   },
+
+  async sendMessageFromAvalancheToNear() {
+    // Cross-chain message delivering from `Avalanche` to `PlatON`. Send greeting to smart contract of `PlatON`
+    await avalanche.sendTransaction(avalancheContract, 'sendGreeting', testAccountPrivateKey, ['NEAR', ['AVALANCHE', 'Greetings', 'Greeting from Avalanche', getCurrentDate()]]);
+  },
+
   async sendOCTaskToAvalanche(nums) {
     // Cross-chain call delivering from `PlatON` to `Avalanche`.
     await platon.sendTransaction(platonContract, 'sendComputeTask', testAccountPrivateKey, ['AVALANCHE', nums]);
   },
+
   async sendOCTaskToPlatON(nums) {
     // Cross-chain call delivering from `Avalanche` to `PlatON`.
     await avalanche.sendTransaction(avalancheContract, 'sendComputeTask', testAccountPrivateKey, ['PlatONEVMDEV', nums]);
