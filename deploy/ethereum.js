cc2daafa166b1d6ded7632de6690fba2851b72ee
virgil2019 <danten0402@gmail.com>
2022-06-20 18:37:36 +0800

    Remove extra files

--- deploy/ethereum.js
+++ deploy/ethereum.js
@@ -1,33 +1,22 @@
 module.exports = {
-  // 通过私钥签名交易
+  // Sign with private key
   async sendTransaction(
     provider, chainId, targetContract, methodName, accountPrivateKey, arguments) {
     provider.eth.handleRevert = true;
     try {
-      const account =
-        provider.eth.accounts.privateKeyToAccount(accountPrivateKey)
-          .address;  // 私钥导出公钥
+      // get pubkey
+      const account = provider.eth.accounts.privateKeyToAccount(accountPrivateKey).address;
       const to = targetContract.options.address;
       const nonce = provider.utils.numberToHex(
-        await provider.eth.getTransactionCount(account));  // 获取生成 nonce
+        await provider.eth.getTransactionCount(account));
       const data = targetContract.methods[methodName]
         .apply(targetContract.methods, arguments)
         .encodeABI();  // encode ABI
-      // const estimateGas =
-      //     await provider.eth.estimateGas({from: account, to, data});
-      const gas = provider.utils.numberToHex(
-        parseInt((await provider.eth.getBlock('latest')).gasLimit - 1));
-      // const gasPrice = await provider.eth.getGasPrice();
       let gasPrice = 50000000000;
-      // console.log('gas: '+gas);
-      // console.log('gasPrice: '+gasPrice);
-      // console.log('estimateGas: ' + estimateGas);
 
-      // 准备交易数据
       const tx = { account, to, chainId, data, nonce, gas: 2000000, gasPrice };
-      // console.log(tx);
 
-      // 签名交易
+      // sign
       let signTx =
         await provider.eth.accounts.signTransaction(tx, accountPrivateKey);
       let ret = await provider.eth.sendSignedTransaction(signTx.rawTransaction);
@@ -42,7 +31,6 @@ module.exports = {
     let methodObj =
       targetContract.methods[method].apply(targetContract.methods, arguments);
     let ret = await methodObj.call({});
-    // console.log(ret);
     return ret;
   }
 }
