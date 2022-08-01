0f2d6f49fa0ad793ee2feae8366b867502074fee
kay404 <kay20475@hotmail.com>
2022-08-01 15:02:07 +0800

    use key service replace .secret file

--- basic/blockchain.js
+++ basic/blockchain.js
@@ -4,7 +4,9 @@ const near = require('./near');
 const ethereum = require('./ethereum');
 const utils = require('./utils');
 
-const ethereumWeb3 = new Web3('https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161');
+const ethereumWeb3 = new Web3(
+  'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
+);
 const moonbeamWeb3 = new Web3('https://moonbase-alpha.public.blastapi.io');
 const fujiWeb3 = new Web3('https://api.avax-test.network/ext/bc/C/rpc');
 
@@ -17,7 +19,9 @@ evmProviders['MOONBASEALPHA'] = [moonbeamWeb3, 1287];
 evmProviders['FUJI'] = [fujiWeb3, 43113];
 
 // Test account
-let testAccountPrivateKey = fs.readFileSync('.secret').toString();
+// let testAccountPrivateKey = fs.readFileSync('.secret').toString();
+let testAccount = '0xA4C1ffF9a99d6Db7bfe2d4DB5eDcC4ED11dfc0aD';
+// let testAccount = '0x04e5d0f5478849C94F02850bFF91113d8F02864D';
 
 // Load smart contract abi
 let greetingRawData = fs.readFileSync('./res/Greetings.json');
@@ -27,37 +31,55 @@ let ocComputeRawData = fs.readFileSync('./res/OCComputing.json');
 let ocComputeAbi = JSON.parse(ocComputeRawData).abi;
 
 // Moonbeam contracts
-let moonbeamGreetingContractAddress = '0x49bC1f09474993103ECa96d96f4C3f7000B5fB7b';
-let moonbeamGreetingContract = new moonbeamWeb3.eth.Contract(greetingAbi, moonbeamGreetingContractAddress);
+let moonbeamGreetingContractAddress =
+  '0x49bC1f09474993103ECa96d96f4C3f7000B5fB7b';
+let moonbeamGreetingContract = new moonbeamWeb3.eth.Contract(
+  greetingAbi,
+  moonbeamGreetingContractAddress
+);
 evmGreetingContracts['MOONBASEALPHA'] = moonbeamGreetingContract;
 
-let moonbeamComputeContractAddress = '0x8c5d8Df81C670b5154fe27930C0289289e94a52f';
-let moonbeamComputeContract = new moonbeamWeb3.eth.Contract(ocComputeAbi, moonbeamComputeContractAddress);
+let moonbeamComputeContractAddress =
+  '0x8c5d8Df81C670b5154fe27930C0289289e94a52f';
+let moonbeamComputeContract = new moonbeamWeb3.eth.Contract(
+  ocComputeAbi,
+  moonbeamComputeContractAddress
+);
 evmComputeContracts['MOONBASEALPHA'] = moonbeamComputeContract;
 
 // Fuji contracts
 let fujiGreetingContractAddress = '0x1723f39e05Ca8b14ACaf244bAFFBd79801d42A63';
-let fujiGreetingContract = new fujiWeb3.eth.Contract(greetingAbi, fujiGreetingContractAddress);
+let fujiGreetingContract = new fujiWeb3.eth.Contract(
+  greetingAbi,
+  fujiGreetingContractAddress
+);
 evmGreetingContracts['FUJI'] = fujiGreetingContract;
 
 let fujiComputeContractAddress = '0x7F5b6F5F7a786F63383E8681Da7ACCEed76Ab209';
-let fujiComputeContract = new fujiWeb3.eth.Contract(ocComputeAbi, fujiComputeContractAddress);
+let fujiComputeContract = new fujiWeb3.eth.Contract(
+  ocComputeAbi,
+  fujiComputeContractAddress
+);
 evmComputeContracts['FUJI'] = fujiComputeContract;
 
 // Rinkeby contracts
 
 // NEAR contract
-let nearContractId = '99ff32da92227f302056389ce208d77e12f88a6ffd2cee1b238586cc4cc20bd7';
-let nearSumContractId = "f738bf496d14e9c2d4b734be2905a319253a7cc5775511434991b9f212224aab";
-let nearSender = "shanks.testnet";
-let nearNetworkId = "testnet";
+let nearContractId =
+  '99ff32da92227f302056389ce208d77e12f88a6ffd2cee1b238586cc4cc20bd7';
+let nearSumContractId =
+  'f738bf496d14e9c2d4b734be2905a319253a7cc5775511434991b9f212224aab';
+let nearSender = 'shanks.testnet';
+let nearNetworkId = 'testnet';
 // const keyFilePath = path.resolve(
 //   homedir,
 //   `./.near-credentials/${nearNetworkId}/${nearSender}.json`
 // );
 // const keyFile = require(keyFilePath);
-const callGreetingPrivateKey = "ed25519:2ujXoT1SktY2tspiAMimY5ZEji1MNdP1fRCUPqzrpdzkkQ7JkPVDg9nS5BV5Yefb2GgHqaE8anC1KfhGLJmmU3Af";
-const callSumPrivateKey = "ed25519:4RPNB4FkrqtEMAm6obq184R5dPrkjRqHBRNuzm1qM1qHMcaxNgbfMcuHyvSVx3HWjxF2hwkrqaGVKMVV5hYj1jV3";
+const callGreetingPrivateKey =
+  'ed25519:2ujXoT1SktY2tspiAMimY5ZEji1MNdP1fRCUPqzrpdzkkQ7JkPVDg9nS5BV5Yefb2GgHqaE8anC1KfhGLJmmU3Af';
+const callSumPrivateKey =
+  'ed25519:4RPNB4FkrqtEMAm6obq184R5dPrkjRqHBRNuzm1qM1qHMcaxNgbfMcuHyvSVx3HWjxF2hwkrqaGVKMVV5hYj1jV3';
 // Get current date
 function getCurrentDate() {
   var today = new Date();
@@ -66,46 +88,114 @@ function getCurrentDate() {
   return ret;
 }
 
-module.exports = {  
+module.exports = {
   async sendMessageFromNearToEthereum(chainName) {
-    return near.sendTransaction(nearContractId, nearSender, callGreetingPrivateKey, "send_greeting", {to_chain: chainName, title: 'Greetings', content: 'Greeting from NEAR', date: getCurrentDate()})
+    return near.sendTransaction(
+      nearContractId,
+      nearSender,
+      callGreetingPrivateKey,
+      'send_greeting',
+      {
+        to_chain: chainName,
+        title: 'Greetings',
+        content: 'Greeting from NEAR',
+        date: getCurrentDate(),
+      }
+    );
   },
 
   async sendOCTaskFromNearToEthereum(chainName, nums) {
-    return near.sendTransaction(nearSumContractId, nearSender, callSumPrivateKey, 'send_compute_task', {to_chain: chainName, nums: nums});
+    return near.sendTransaction(
+      nearSumContractId,
+      nearSender,
+      callSumPrivateKey,
+      'send_compute_task',
+      { to_chain: chainName, nums: nums }
+    );
   },
-  
+
   async queryMessageFromNear(chainName, id) {
-    const message = await near.contractCall(nearContractId, "get_greeting", { "from_chain": chainName, "id": id });
+    const message = await near.contractCall(nearContractId, 'get_greeting', {
+      from_chain: chainName,
+      id: id,
+    });
     return message;
   },
-  
+
   async queryOCResultFromNear(chainName, id) {
-    const message = await near.contractCall(nearSumContractId, 'get_compute_task', {to_chain: chainName, id: id});
+    const message = await near.contractCall(
+      nearSumContractId,
+      'get_compute_task',
+      { to_chain: chainName, id: id }
+    );
     return message;
   },
 
   async sendMessageFromEthereum(fromChain, toChain) {
-    await ethereum.sendTransaction(evmProviders[fromChain][0], evmProviders[fromChain][1], evmGreetingContracts[fromChain], 'sendGreeting', testAccountPrivateKey, [toChain, [fromChain, 'Greetings', 'Greeting from ' + fromChain, getCurrentDate()]]);
+    // console.log(evmProviders[fromChain][0].currentProvider.host);
+    // return ;
+    await ethereum.sendTransaction(
+      fromChain,
+      evmProviders[fromChain][0],
+      evmProviders[fromChain][1],
+      evmGreetingContracts[fromChain],
+      'sendGreeting',
+      testAccount,
+      [
+        toChain,
+        [
+          fromChain,
+          'Greetings',
+          'Greeting from ' + fromChain,
+          getCurrentDate(),
+        ],
+      ]
+    );
     await utils.sleep(5);
-    let id = await ethereum.contractCall(evmGreetingContracts[fromChain], 'currentId', []);
-    return id;
+    if (fromChain != 'FUJI') {
+      let id = await ethereum.contractCall(
+        evmGreetingContracts[fromChain],
+        'currentId',
+        []
+      );
+      return id;
+    };
   },
 
   async sendOCTaskFromEthereum(fromChain, toChain, nums) {
-    await ethereum.sendTransaction(evmProviders[fromChain][0], evmProviders[fromChain][1], evmComputeContracts[fromChain], 'sendComputeTask', testAccountPrivateKey, [toChain, nums]);
+    await ethereum.sendTransaction(
+      fromChain,
+      evmProviders[fromChain][0],
+      evmProviders[fromChain][1],
+      evmComputeContracts[fromChain],
+      'sendComputeTask',
+      testAccount,
+      [toChain, nums]
+    );
     await utils.sleep(5);
-    let id = await ethereum.contractCall(evmComputeContracts[fromChain], 'currentId', []);
+    let id = await ethereum.contractCall(
+      evmComputeContracts[fromChain],
+      'currentId',
+      []
+    );
     return id;
   },
-  
+
   async queryMessageFromEthereum(chainName, fromChain, id) {
-    const message = await ethereum.contractCall(evmGreetingContracts[chainName], 'greetings', [fromChain, id]);
+    const message = await ethereum.contractCall(
+      evmGreetingContracts[chainName],
+      'greetings',
+      [fromChain, id]
+    );
     return message;
   },
-  
+
   async queryOCResultFromEthereum(chainName, fromChain, id) {
-    const message = await ethereum.contractCall(evmComputeContracts[chainName], 'ocResult', [fromChain, id]);
+    const message = await ethereum.contractCall(
+      evmComputeContracts[chainName],
+      'ocResult',
+      [fromChain, id]
+    );
     return message;
   },
-}
+};
--- basic/utils.js
+++ basic/utils.js
@@ -1,13 +1,33 @@
 'use strict';
 
+const request = require('request');
 async function sleep(seconds) {
-    await new Promise((resolve) => {
-        setTimeout(() => {
-        resolve();
-        }, seconds * 1000);
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000);
+  });
+}
+
+async function syncRequest(url, method, body) {
+  let options = {
+    url: url,
+    method: method,
+    json: true,
+    body: body,
+  };
+  return new Promise(function (resolve, reject) {
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
     });
+  });
 }
 
 module.exports = {
-    sleep: sleep,
-}
+  sleep: sleep,
+  request: syncRequest,
+};
